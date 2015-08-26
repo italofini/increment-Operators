f605dc75a3f2713b23874b6fe1f228b0963416b7
italofini <italo.fini@techhigh.us>
2015-08-25 23:57:50 -0400

    IO

---
+++ src/Bannas.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Bannas {
+	public static void main(String args[]){
+		Scanner bucky = new Scanner (System.in);
+		
+		int tuna = 5;
+		int bass = 18;
+		
+		tuna +=8;
+		
+		System.out.println(tuna);
+		
+	}
+
+}
